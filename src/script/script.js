9092bf89df5c50c1641aa9e742fcb687d1a1f818
Masaki M <mmrsk@mac.com>
2025-12-22 23:43:21 -0800

    Initial commit

---
+++ src/script/script.js
@@ -0,0 +1,16 @@
+console.log(`whassup gsap?`);
+
+gsap.set(".layer", { opacity: 0 });
+gsap.set("#ir_logo", { opacity: 0 });
+
+gsap.registerPlugin(ScrambleTextPlugin);
+
+gsap.to(".tagline_0", { duration: 1, chars: "upperAndLowerCase", scrambleText: "Your" });
+gsap.to(".tagline_1", { duration: 1.25, chars: "upperAndLowerCase", scrambleText: "Data." });
+gsap.to(".tagline_2", { duration: 1.25, chars: "upperAndLowerCase", scrambleText: "Business." });
+gsap.to(".tagline_3", { duration: 1.25, chars: "upperAndLowerCase", scrambleText: "Story." });
+gsap.to(".tagline", { opacity: 0, delay: 4, duration: 1 });
+
+gsap.to(".layer", { opacity: 0.4, delay: 2, duration: 2, stagger: { each: 0.03, ease: "power2.inOut" } });
+gsap.to("#ir_symbol", { x: -269, y: -0.5,  scale: 0.305, rotate: 360, opacity: 1, delay: 4, duration: 2 });
+gsap.to("#ir_logo", { opacity: 1, delay: 6, duration: 2, duration: 1 });
